1715cdf95b40f3228a44fd94d5109eeb05952cf5
holyshared <holy.shared.design@gmail.com>
2015-10-24 14:44:14 +0900

    Add source header

--- src/Stream.hh
+++ src/Stream.hh
@@ -1,5 +1,14 @@
 <?hh // strict
 
+/**
+ * This file is part of hhpack\process package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace hhpack\process;
 
 interface Stream
