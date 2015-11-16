bc7068398d1c60e55bd24ad650cc48c8988fcea9
holyshared <holy.shared.design@gmail.com>
2015-11-16 22:00:51 +0900

    Update example

--- example/typecheck.hh
+++ example/typecheck.hh
@@ -4,15 +4,14 @@ namespace hhpack\process\example;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use hhpack\process\Process;
-use hhpack\process\GeneralContext;
+use hhpack\process;
 
 async function main() : Awaitable<void>
 {
-    $result = await Process::exec('hh_client restart');
+    $result = await process\exec('hh_client restart');
     $result->display();
 
-    $result = await Process::exec('hh_client check --json');
+    $result = await process\exec('hh_client check --json');
     $result->display();
 }
 
