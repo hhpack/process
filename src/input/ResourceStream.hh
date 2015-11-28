71c8bfbbfa5f9c18dbde09ee23b0b1ac0b2af61b
holyshared <holy.shared.design@gmail.com>
2015-11-28 17:41:49 +0900

    add resource stream

---
+++ src/input/ResourceStream.hh
@@ -0,0 +1,86 @@
+<?hh // strict
+
+/**
+ * This file is part of hhpack\process package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace hhpack\process\input;
+
+final class ResourceStream implements ReadableStream<int>
+{
+
+    public function __construct(
+        private resource $handle
+    )
+    {
+        stream_set_blocking($this->handle, 0);
+    }
+
+    public function eof() : bool
+    {
+        return feof($this->handle);
+    }
+
+    public function isOpened() : bool
+    {
+        return is_resource($this->handle);
+    }
+
+    public function isClosed() : bool
+    {
+        return $this->isOpened() === false;
+    }
+
+    public function ready() : bool
+    {
+        $read = [ $this->handle ];
+        $write = [];
+        $expect = null;
+
+        $ng = ($num = stream_select($read, $write, $expect, 0, 200000)) === false;
+
+        if ($ng || $num <= 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function notReady() : bool
+    {
+        return $this->ready() === false;
+    }
+
+    public function read(int $length = 4096) : string
+    {
+        if ($this->notReady()) {
+            return '';
+        }
+
+        $bufferedOutput = '';
+
+        while (($chunk = fread($this->handle, 16384)) !== false) {
+            if ((string) $chunk === '') {
+                break;
+            }
+            $bufferedOutput .= (string) $chunk;
+        }
+
+        if ($this->eof() && strlen($bufferedOutput) <= 0) {
+            $this->close();
+        }
+
+        return $bufferedOutput;
+    }
+
+    public function close() : void
+    {
+        fclose($this->handle);
+    }
+
+}
